8f6117ae8c258c8bf2e0c37f92ae764937d6aa17
NicoGER1 <nico3g@hotmail.fr>
2024-07-04 15:01:12 +0200

    header style ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,30 @@ import "../styles/Home.css";
 import CategorieComponent from "../components/CategorieComponent";
 import logoWhite from "../assets/Default_white.png";
 import About from "../components/About";
-import pictCat from "../assets/react.svg";
+
 function Home() {
   return (
     <>
       <section className="HomeComponent">
-        <div>
-          <img src={logoWhite} alt="logo en blanc Harmoni" />
-          <h1>Créez un environnement de travail inclusif</h1>
-          <p>
-            Bienvenue sur notre plateforme dédiée à l&apos;inclusion et à la
-            diversité en entreprise. Nous aidons les entreprises à adopter des
-            pratiques inclusives et à promouvoir la diversité. Découvrez comment
-            nos services peuvent transformer votre culture d&apos;entreprise.
-          </p>
-          <a href="#CategorieComponent">
-            <button>Découvrir nos conseils</button>
-          </a>
-        </div>
-        <img src={pictCat} alt="pic-cat" />
-        <About />
+        <img
+          src={logoWhite}
+          alt="logo en blanc Harmoni"
+          className="logo-header"
+        />
+        <h1 className="title-header">
+          Créez un environnement de travail inclusif
+        </h1>
+        <p className="para-header">
+          Bienvenue sur notre plateforme dédiée à l&apos;inclusion et à la
+          diversité en entreprise. Nous aidons les entreprises à adopter des
+          pratiques inclusives et à promouvoir la diversité. Découvrez comment
+          nos services peuvent transformer votre culture d&apos;entreprise.
+        </p>
+        <a href="#CategorieComponent">
+          <button className="button-header">Découvrir nos conseils</button>
+        </a>
       </section>
+      <About />
       <CategorieComponent />
     </>
   );
